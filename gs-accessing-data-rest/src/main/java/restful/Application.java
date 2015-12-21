06fe62d6c757a07ae45da5ed68ceba396d67af00
cvsuser-chromium <cvsuser@gmail.com>
2015-12-21 23:16:28 +0800

    tomecat listening in http(8080) and https(8443).

--- gs-accessing-data-rest/src/main/java/restful/Application.java
+++ gs-accessing-data-rest/src/main/java/restful/Application.java
@@ -93,17 +93,17 @@ public class Application {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
 		try {
-			File keystore = new ClassPathResource("keystore").getFile();
-			File truststore = new ClassPathResource("keystore").getFile();
+			File keystore = new ClassPathResource("tomcat.keystore").getFile();
+			//File truststore = new ClassPathResource("keystore").getFile();
 			connector.setScheme("https");
 			connector.setSecure(true);
 			connector.setPort(8443);
 			protocol.setSSLEnabled(true);
 			protocol.setKeystoreFile(keystore.getAbsolutePath());
-			protocol.setKeystorePass("changeit");
-			protocol.setTruststoreFile(truststore.getAbsolutePath());
-			protocol.setTruststorePass("changeit");
-			protocol.setKeyAlias("apitester");
+			protocol.setKeystorePass("123456");
+			//protocol.setTruststoreFile(truststore.getAbsolutePath());
+			//protocol.setTruststorePass("changeit");
+			//protocol.setKeyAlias("apitester");
 			return connector;
 		} catch (IOException ex) {
 			throw new IllegalStateException(
