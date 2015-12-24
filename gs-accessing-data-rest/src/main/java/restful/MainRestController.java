c012d0bc2334bde156b03474b1a368606216fb90
cvsuser@gmail.com <cvsuser@gmail.com>
2015-12-24 18:17:03 +0800

    tidying code.

--- gs-accessing-data-rest/src/main/java/restful/MainRestController.java
+++ gs-accessing-data-rest/src/main/java/restful/MainRestController.java
@@ -2,9 +2,7 @@ package restful;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.Filter;
@@ -18,36 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.CompositeFilter;
 
 @Configuration
 @RestController
-@EnableOAuth2Client
-@EnableAuthorizationServer
-public class MainRestController extends WebSecurityConfigurerAdapter {
+public class MainRestController {
 
 	@Autowired
 	OAuth2ClientContext oauth2ClientContext;
@@ -57,19 +39,6 @@ public class MainRestController extends WebSecurityConfigurerAdapter {
 		Map<String, String> map = new LinkedHashMap<>();
 		map.put("name", principal.getName());
 		return map;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http.antMatcher("/**").authorizeRequests().anyRequest().anonymous()
-				.antMatchers("/health*", "/beans*", "mapping*").permitAll().antMatchers("/", "/login**", "/webjars/**")
-				.permitAll().anyRequest().authenticated()
-				.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and()
-				.logout().logoutSuccessUrl("/").permitAll().and()
-				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-
-		// @formatter:on
 	}
 
 	@Configuration
@@ -113,65 +82,5 @@ public class MainRestController extends WebSecurityConfigurerAdapter {
 			public void destroy() {
 			}
 		});
-	}
-
-	@Bean
-	@ConfigurationProperties("sinaweibo")
-	ClientResources sinaweibo() {
-		return new ClientResources();
-	}
-
-	@Bean
-	@ConfigurationProperties("facebook")
-	ClientResources facebook() {
-		return new ClientResources();
-	}
-
-	@Bean
-	@ConfigurationProperties("github")
-	ClientResources github() {
-		return new ClientResources();
-	}
-
-	private Filter ssoFilter() {
-		CompositeFilter filter = new CompositeFilter();
-		List<Filter> filters = new ArrayList<>();
-		filters.add(ssoFilter(facebook(), "/login/facebook"));
-		filters.add(ssoFilter(github(), "/login/github"));
-		filters.add(ssoFilter(sinaweibo(), "/login/sinaweibo"));
-		filter.setFilters(filters);
-		return filter;
-	}
-
-	private Filter ssoFilter(ClientResources client, String path) {
-		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
-				path);
-		OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
-		filter.setRestTemplate(template);
-		filter.setTokenServices(
-				new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId()));
-		return filter;
-	}
-
-	@Bean
-	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(filter);
-		registration.setOrder(-100);
-		return registration;
-	}
-
-}
-
-class ClientResources {
-	private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
-	private ResourceServerProperties resource = new ResourceServerProperties();
-
-	public OAuth2ProtectedResourceDetails getClient() {
-		return client;
-	}
-
-	public ResourceServerProperties getResource() {
-		return resource;
 	}
 }
